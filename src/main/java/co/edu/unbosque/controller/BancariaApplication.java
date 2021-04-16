d7c97541fffeb05f6260d8d9a74460e5865ff838
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-04-16 18:03:58 -0500

    Fixes on main controller | Gabriel Blanco

--- src/main/java/co/edu/unbosque/controller/BancariaApplication.java
+++ src/main/java/co/edu/unbosque/controller/BancariaApplication.java
@@ -3,7 +3,6 @@ package co.edu.unbosque.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"co.edu.unbosque.model"})
--- src/main/java/co/edu/unbosque/controller/BancariaController.java
+++ src/main/java/co/edu/unbosque/controller/BancariaController.java
@@ -1,11 +1,14 @@
 package co.edu.unbosque.controller;
 
 import co.edu.unbosque.model.AgenciaDAO;
+import co.edu.unbosque.model.persistence.AgenciaDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @Slf4j
@@ -13,9 +16,10 @@ public class BancariaController {
     @Autowired
     private AgenciaDAO agencia;
     @GetMapping("/")
-    public String downloadAgencies(Model model) {
+    public String bancaria(Model model) {
         log.info("Iniciando REST");
-        var agencias = agencia.findAll();
+        var agencias = (ArrayList<AgenciaDTO>) agencia.findAll();
+        log.info("Agencias: "+agencias.size());
         model.addAttribute("agencias", agencias);
         return "index";
     }
